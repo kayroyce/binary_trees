49fa9335887f96ac2c117b90ebb4c08a0d007478
kayroyce <kayroyce2005@gmail.com>
2022-09-19 15:21:49 +0100

    task 0 & 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *node;
+    //node - malloc(sizeof(binary_tree_t));
+    
+    if (parent_node == NULL)
+        {
+            return (NULL);
+        }
+
+    node = binary_tree_node(parent, value);
+    if (node == NULL)
+    {
+        return (NULL);
+    }
+
+    if (parent ->left != NULL)
+    {
+        node ->left = parent ->left;
+        parent ->left -> parent = node;
+    }
+
+    parent->left = node;
+    return (new);
+
+}
