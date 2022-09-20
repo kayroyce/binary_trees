aa726aa1b162e0b393cfe1038c5bf3adfc9e2fe4
kayroyce <kayroyce2005@gmail.com>
2022-09-19 22:47:33 -0700

    task 2-4

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
-{
-    binary_tree_t *new;
+{	
+    binary_tree_t *node;
 
     if (parent == NULL)
     {
             return(NULL);
     }
     
-    node = binary_tree_delete(parent, value)
+    node = binary_tree_node(parent, value);
     if (node == NULL)
     {
             return(NULL);
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -4,11 +4,15 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
     if(node == NULL)
     {
-            return (NULL)
+            return (0);
     }
-    if (if node-> left == NULL && node-> right == NULL)
+    if (node-> left == NULL)
     {
-            return(1)
+            return(1);
     }
-    return(0)
+    if (node-> right == NULL)
+    {
+	    return(1);
+    }
+    return(0);
 }
