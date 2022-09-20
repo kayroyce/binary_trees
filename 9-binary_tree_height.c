9023f59a614ea4ca17cadf0229bdae15fd29c93d
kayroyce <kayroyce2005@gmail.com>
2022-09-20 06:30:47 -0700

    task 4-9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
         {
                 if(tree)
                 {
-                        left = tree-> left ? 1 + binary_tree_height(tree-> left) : 0;t
+                        left = tree-> left ? 1 + binary_tree_height(tree-> left) : 0;
                         right = tree-> right ? + binary_tree_height(tree-> right) : 0;
                 }
                 return((1 > left) ? left : right);
